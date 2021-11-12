7489a7078fee546d8a9cccd6bdda4368c7a915d9
Aston Bailey-Fong <aston.bailey-fong@education.nsw.gov.au>
2021-11-12 14:21:01 +1100

    Added in V1.04.

--- VB-Pizza-v102/My Project/Application.Designer.vb
+++ VB-Pizza-v102/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Pizza102.Pizza102
+            Me.MainForm = Global.Pizza102.Pizza101
         End Sub
     End Class
 End Namespace
--- VB-Pizza-v104/My Project/Application.Designer.vb
+++ VB-Pizza-v104/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.StDBv106.StDB106
+            Me.MainForm = Global.Pizza103.Pizza103
         End Sub
     End Class
 End Namespace
